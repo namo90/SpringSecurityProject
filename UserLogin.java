40615593a0ddccf49f4339e9817e3e98a691c2ee
namo90 <85549230+namo90@users.noreply.github.com>
2021-06-15 11:15:43 +0530

    Update UserLogin.java

--- UserLogin.java
+++ UserLogin.java
@@ -1,9 +1,8 @@
 package com.Practice.onlineShoppingProject.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.Practice.onlineShoppingProject.Entites.Login;
 import com.Practice.onlineShoppingProject.Repo.LoginRepository;
 
@@ -14,14 +13,14 @@ public class UserLogin {
 	LoginRepository loginRepository;
 	@Autowired
 	Login login;
-	@RequestMapping("/login1")
+	@GetMapping("/checkauth/{name}{password}") 
 	public String checkAuthanication(String name,String password)
 	{
 	
 		return "";
 		
 	}
-	@RequestMapping("/welcome")
+	@GetMapping("/welcome")
 	public String welcome()
 	{
 		String text="this is privatepage";
